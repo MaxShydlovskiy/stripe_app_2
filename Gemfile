e45820610b8f001c1672a51f1f984950224b09f5
Max Shydlovskiy <shidlymax@gmail.com>
2023-01-27 16:23:32 +0200

    Final commit

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'devise'
 gem 'stripe'
+gem 'dotenv-rails', :groups => [:development, :test]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :charges, only: [:new, :create]
   devise_for :users
   resources :posts
   root to: 'posts#index'
+  get 'thanks', to: 'charges#thanks', as: 'thanks'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
